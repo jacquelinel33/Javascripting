3b2855b4aed2542e2ad336cec698a4fbd992f665
Jacqueline Lee <jlee4332@gmail.com>
2021-02-01 16:23:09 +0000

    Initial commit with all my work so far

---
+++ looping-through-arrays.js
@@ -0,0 +1,5 @@
+const pet = ['cat', 'dog', 'rat']
+for (i=0; i<3; i++) {
+  pet[i] = pet[i] + 's';
+}
+console.log(pet)
---
+++ rounding-numbers.js
@@ -0,0 +1,3 @@
+let og = 1.5
+let round = Math.round(og);
+console.log(round);
